3ffba64e6b8d246395ca0c33cdd890726799a36b
guilherme-paiva2007 <guilhermepaiva2007h@gmail.com>
2025-11-04 09:01:33 -0300

    app.tsx nao foi no commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,25 @@
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { screens } from './src/features/navigation';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator>
+        {(Object.entries(screens).map(([name, config]) =>
+          <Drawer.Screen
+            key={"Screen." + name}
+            name={name}
+            component={config.component}
+            options={config.options}
+          />
+        ))}
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
